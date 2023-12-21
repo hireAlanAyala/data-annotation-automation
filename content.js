5db822b3766fb413d4256f70c21a94fbaa034c17
kaden <kadenfranklin@hotmail.com>
2023-12-21 17:13:32 -0600

    webscraping

--- content.js
+++ content.js
@@ -5,7 +5,29 @@ function scrapeContent() {
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.action === "scrape") {
-        scrapeContent();
+        scrapequery();
         sendResponse({status: "Content scraped"});
     }
 });
+
+function scrapequery() {
+    // const instructions = document.querySelector('[data-testid="instructions"]').textContent;
+
+    const fields = document.querySelector('[data-testid="fields-text"]').textContent;
+
+    const question_elements = document.querySelectorAll('[data-testid^="question-"]');
+    
+    const questions = [];
+
+    for (const elem of question_elements) {
+  
+        var regex = /^question-\d+$/;
+        var testDataId = elem.getAttribute('data-testid');
+
+        if (regex.test(testDataId)) {
+           questions.push(elem.textContent);
+        }
+
+    }
+    console.log(questions);
+}
